a12f9f444b1705a6b1a46ce7c47d2851a261e7e9
tonyynot <alramella@gmail.com>
2016-12-05 19:21:07 -0500

    tests passing for user profile access

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
 	def setup
-		@user = users(:anthony)
+		@user 		= users(:anthony)
+		@other_user = users(:archer)
 	end
 
 	test "should get new" do
@@ -13,11 +15,26 @@ class UsersControllerTest < ActionController::TestCase
 	test "should redirect edit when not logged in" do
 		get :edit, id: @user
 		assert_redirected_to login_url
+		assert_not flash.empty?
 	end
 
 	test "should redirect update when not logged in" do
 		patch :update, id: @user, user: { name: @user, email: @user.email }
+		assert_not flash.empty?
 		assert_redirected_to login_url
 	end
 
+	test "should redirect edit when logged in as wrong user" do
+		log_in_as(@other_user)
+		get :edit, id: @user
+		assert flash.empty?
+		assert_redirected_to root_url
+	end
+
+	test "should redirect update when logged in as wrong user" do
+		log_in_as(@other_user)
+		patch :update, id: @user, user: { name: @user, email: @user.email }
+		assert flash.empty?
+		assert_redirected_to root_url
+	end
 end
